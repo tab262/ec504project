e6175c2a5787a596e7623cbc87d966c49e71a0e8
gaddis <gaddis@gaddis-700Z7C>
2014-04-21 15:04:08 -0400

    d

--- src/jpeg/JPEGtoRaw2.java
+++ src/jpeg/JPEGtoRaw2.java
@@ -1,3 +1,6 @@
+//http://stackoverflow.com/questions/10923842/can-i-change-the-compression-algorithm-used-by-javas-imagewriter-when-creating
+/*taken from: http://www.programcreek.com/2009/02/java-convert-image-to-byte-array-convert-byte-array-to-image/ for learning purposes*/
+
 package jpeg;
 
 import java.awt.image.BufferedImage;
@@ -7,6 +10,8 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
 
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
  
@@ -15,21 +20,40 @@ public class JPEGtoRaw2 {
 	public static void main(String[] args) throws IOException{
 		String dirName = System.getProperty("user.dir");
 		
-		String fileName = dirName + "/data/img2.jpg";
+		String fileName = dirName + "/data/test.jpeg";
 		System.out.println(fileName);
+
+		ImageWriter writer = ImageIO.getImageWritersByFormatName("jpeg").next();
+		ImageWriteParam iwp = writer.getDefaultWriteParam();
+		iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		iwp.setCompressionQuality(.75f);
 		
 
 		ByteArrayOutputStream baos=new ByteArrayOutputStream(1000);
-		BufferedImage img=ImageIO.read(new File(fileName));
+		
+		BufferedImage img=ImageIO.read(new File(fileName));		
+		
+		
+		System.out.println("IMG: " + img.getHeight() + "x" + img.getWidth());
+		
 		ImageIO.write(img, "jpg", baos);
+		
+		
+		System.out.println("Number of Bytes In image: " + baos.size());
 		baos.flush();
  
 		String base64String=Base64.encode(baos.toByteArray());
+		System.out.println("String: " + base64String.length());
 		baos.close();
  
 		byte[] bytearray = Base64.decode(base64String);
- 
+		System.out.println("BYTEARRAY: " + bytearray.length);
 		BufferedImage imag=ImageIO.read(new ByteArrayInputStream(bytearray));
-		ImageIO.write(imag, "jpg", new File(dirName+"/data/","snap.jpg"));
+		System.out.println("IMAG: " + imag.getHeight() + "x" + imag.getWidth());
+		ImageIO.write(imag, "jpg", new File(dirName+"/data/","test2.jpeg"));
+	}
+	
+	public static void testCompression(String fileName){
+		
 	}
 }

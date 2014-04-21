db9c491adb5d4ed72528c9ac489622a4da6ab724
gaddis <gaddis@gaddis-700Z7C>
2014-04-21 15:24:06 -0400

    Added testCompression to src > JPEGtoRaw2, which has a parameter inside the method (float quality) that alls one to set the compression level of the photo.

--- src/jpeg/JPEGtoRaw2.java
+++ src/jpeg/JPEGtoRaw2.java
@@ -1,34 +1,86 @@
 //http://stackoverflow.com/questions/10923842/can-i-change-the-compression-algorithm-used-by-javas-imagewriter-when-creating
 /*taken from: http://www.programcreek.com/2009/02/java-convert-image-to-byte-array-convert-byte-array-to-image/ for learning purposes*/
-
+//http://examples.javacodegeeks.com/desktop-java/imageio/compress-a-jpeg-file/
 package jpeg;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
  
  
 public class JPEGtoRaw2 {
 	public static void main(String[] args) throws IOException{
-		String dirName = System.getProperty("user.dir");
 		
+		String dirName = System.getProperty("user.dir");
 		String fileName = dirName + "/data/test.jpeg";
 		System.out.println(fileName);
 
-		ImageWriter writer = ImageIO.getImageWritersByFormatName("jpeg").next();
-		ImageWriteParam iwp = writer.getDefaultWriteParam();
-		iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-		iwp.setCompressionQuality(.75f);
-		
+		testCompression(fileName);
+	}
+	
+	public static void testCompression(String fileName) throws IOException{
+		File imageFile = new File(fileName);
+		File compressedImageFile = new File(fileName + "_out");
 
+		InputStream is = new FileInputStream(imageFile);
+		OutputStream os = new FileOutputStream(compressedImageFile);
+
+		// A quality of .86 gets you the same size back
+		// A quality of 1.0 cause the file to grow in size 
+		float quality = .859f;
+		
+		// create a BufferedImage as the result of decoding the supplied InputStream
+		BufferedImage image = ImageIO.read(is);
+
+				// get all image writers for JPG format
+		Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+
+		if (!writers.hasNext())
+			throw new IllegalStateException("No writers found");
+
+		ImageWriter writer = (ImageWriter) writers.next();
+		ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+		writer.setOutput(ios);
+
+		ImageWriteParam param = writer.getDefaultWriteParam();
+
+		// compress to a given quality
+		param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		param.setCompressionQuality(quality);
+
+		// appends a complete image stream containing a single image and
+		//associated stream and image metadata and thumbnails to the output
+		writer.write(null, new IIOImage(image, null, null), param);
+
+		// close all streams
+		is.close();
+		os.close();
+		ios.close();
+		writer.dispose();
+
+}
+		
+	
+	
+	public static void generalCompression(String fileName) throws IOException{
+		
+		String dirName = System.getProperty("user.dir");
+		
 		ByteArrayOutputStream baos=new ByteArrayOutputStream(1000);
 		
 		BufferedImage img=ImageIO.read(new File(fileName));		
@@ -51,9 +103,5 @@ public class JPEGtoRaw2 {
 		BufferedImage imag=ImageIO.read(new ByteArrayInputStream(bytearray));
 		System.out.println("IMAG: " + imag.getHeight() + "x" + imag.getWidth());
 		ImageIO.write(imag, "jpg", new File(dirName+"/data/","test2.jpeg"));
-	}
-	
-	public static void testCompression(String fileName){
-		
 	}
 }

ec2c0393c5d56bb26a13436546534f424086a41d
tab262 <caseface31@gmail.com>
2014-04-22 18:52:18 -0400

    rudimentary player working...

--- src/movie/Player.java
+++ src/movie/Player.java
@@ -1,16 +1,23 @@
 package movie;
 
+import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Player {
 
 	float timePerFrame;
 	Movie m;
 	BufferedImage images[];
-
+	ImageIcon icons[];
 	public void openMovie(String fileName){
 		this.m = null;
 
@@ -34,15 +41,46 @@ public class Player {
 	}
 	
 	
-	public void buildBufferedImages(Frame[] f){
+	public void buildBufferedImages(Frame[] f) throws IOException{
 		images = new BufferedImage[f.length];
+		icons = new ImageIcon[f.length];
+		for(int i = 0; i < f.length;i++){
+			images[i] = ImageIO.read(new ByteArrayInputStream(f[i].bytearray));
+			icons[i] = new ImageIcon(images[i]);
+		}
 	}
 	
-	public static void main(String[] args){
+	public void playMovie() throws InterruptedException{
+		int height = m.frames[0].height;
+		int width = m.frames[0].width;
+		
+		ImageIcon icon;
+		JFrame frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		frame.setSize(height,width);
+		JLabel lbl = new JLabel();
+		lbl.setIcon(this.icons[0]);
+		frame.add(lbl);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		int i = 0;
+		while(i < 100){
+			icon = (this.icons[i%m.frames.length]);
+			Thread.sleep(250);
+			lbl.setIcon(icon);
+			frame.add(lbl);
+			i++;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException{
 		Player p = new Player();
 		p.openMovie("/home/gaddis/git/ec504project/data/sequence/movies/movie1.ser");
 		System.out.println("This movie has " + p.m.frames.length + " frames");
 		Frame[] f = p.m.frames;
+		p.buildBufferedImages(p.m.frames);
+		p.playMovie();
 	}
 	
 }

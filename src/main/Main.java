7021acdb1a3f10344bdb423781ed26d767d6c241
g <g@wireless1x-155-41-69-134.bu.edu>
2014-04-23 18:39:50 -0400

    command line

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,19 +16,30 @@ public class Main {
 		File folder = new File(directory);
 		File[] listOfFiles = folder.listFiles();
 		Arrays.sort(listOfFiles);
+		String fileNames[] = new String[listOfFiles.length];
 		//http://stackoverflow.com/questions/5694385/getting-the-filenames-of-all-files-in-a-folder
 	    for (int i = 0; i < listOfFiles.length; i++) {
 	        if (listOfFiles[i].isFile()) {
-	          System.out.println("File: " + listOfFiles[i].getName());
+	          //System.out.println("File: " + listOfFiles[i].getName());
+	          fileNames[i] = listOfFiles[i].getAbsolutePath();
 	        }
-	      }
+	    }
+	    
+	    for(int i = 0; i < fileNames.length;i++){
+	    	System.out.println(fileNames[i]);
+	    }
+	    
 	}
 	
 	public static void fileMode(String[] args){
 		System.out.println("File Mode: ");
 		int i;
+		String fileNames[] = new String[args.length - 1];
 		for(i = 1; i < args.length;i++){
-			System.out.println(args[i]);
+			fileNames[i-1] = args[i]; 
+		}
+		for(i = 0 ; i < fileNames.length;i++){
+			System.out.println(fileNames[i]);
 		}
 	}
 	
